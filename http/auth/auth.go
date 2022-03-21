9558b073dc3ad209b91812687bdf37d08b9ca197
Dennis Walters <dwalters@starkandwayne.com>
2022-03-21 14:17:13 -0400

    Implemented Just Enough Auth (TM)

--- http/auth/auth.go
+++ http/auth/auth.go
@@ -3,11 +3,20 @@ package auth
 import (
 	"fmt"
 
-	"github.com/labstack/echo"
+	"github.com/labstack/echo/v4"
 )
 
 func Verify(c echo.Context) error {
 	auth := c.Request().Header.Get(echo.HeaderAuthorization)
+	fmt.Println("(DEBUG) auth:", auth)
+	if len(auth) == 0 {
+		return fmt.Errorf("no auth provided")
+	}
 
-	return fmt.Errorf("unimplemented")
+	if auth != "jeremy" {
+		return fmt.Errorf("you're not jeremy!")
+	}
+
+	//return fmt.Errorf("unimplemented")
+	return nil
 }
