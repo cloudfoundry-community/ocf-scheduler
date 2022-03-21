f613e7ccb19b4a22f7af9c883768f7e6ae6009f4
Dennis Walters <dwalters@starkandwayne.com>
2022-03-21 14:25:32 -0400

    Wired the http server into the executable

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/starkandwayne/scheduler-for-ocf/core"
+	"github.com/starkandwayne/scheduler-for-ocf/http"
+	"github.com/starkandwayne/scheduler-for-ocf/mock"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	jobs := mock.NewJobService()
+	services := &core.Services{Jobs: jobs}
+	server := http.Server("0.0.0.0:8000", services)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			fmt.Println("stopping the server")
+		}
+	}()
+
+	fmt.Println("Listening for connections on", server.Addr)
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		server.Close()
+		fmt.Println(err)
+		os.Exit(2)
+	}
 }
