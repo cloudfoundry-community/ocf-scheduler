3cc4b581bdc5b24c06ba66aec60e2ec4d546d64d
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2024-06-10 10:56:42 +0000

    feat(authservice): Add Logger interface & integr8
    - Introduced a Logger interface for structured
    logging. @itsouvalas
    - Updated AuthService to include a logger for
    better logging practices. @itsouvalas
    - Added logging for start of verification process,
     errors, and obtained information. @itsouvalas
    - Updated the instantiation of AuthService in the
    scheduler to pass the logger instance. @itsouvalas

--- cf/auth_service.go
+++ cf/auth_service.go
@@ -11,55 +11,85 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type AuthService struct {
-	client *cf.Client
+type Logger interface {
+	Info(tag, message string)
+	Error(tag, message string)
 }
 
-func NewAuthService(client *cf.Client) *AuthService {
+type AuthService struct {
+	client *cf.Client
+	logger Logger
+}
+
+func NewAuthService(client *cf.Client, logger Logger) *AuthService {
 	return &AuthService{
 		client: client,
+		logger: logger,
 	}
 }
 
 func (service *AuthService) Verify(auth string) error {
+	tag := "AuthService.Verify"
+	service.logger.Info(tag, "Starting verification process")
 	username, err := getUsername(auth)
 	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error getting username: %v", err))
 		return err
 	}
+	service.logger.Info(tag, fmt.Sprintf("Username obtained: %s", username))
 
 	user, err := service.getUser(username)
 	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error getting user: %v", err))
 		return err
 	}
+	//  Debugging = noisy
+	//	service.logger.Info(tag, fmt.Sprintf("User obtained: %v", user))
 
 	roles, err := service.getUserRoles(user)
 	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error getting user roles: %v", err))
 		return err
 	}
+	//  Debugging = noisy
+	// service.logger.Info(tag, fmt.Sprintf("User roles obtained: %v", roles))
+
+	tokenScopes, err := getTokenScopes(auth, service.logger)
+	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error getting token scopes: %v", err))
+		return err
+	}
+	service.logger.Info(tag, fmt.Sprintf("Token scopes obtained: %v", tokenScopes))
 
 	// Check all the roles, but return good early if we find one that works.
+	// Check CF roles for space_manager or space_developer
 	for _, role := range roles {
 		// NOTE: we should definitely be checking space IDs, too, but that's tomorrow
 		// guy's problem.
 		if role.Type == "space_manager" || role.Type == "space_developer" {
+			service.logger.Info(tag, fmt.Sprintf("User has role: %s", role.Type))
 			return nil
 		}
 	}
 
+	service.logger.Error(tag, "User does not have sufficient permissions")
 	return fmt.Errorf("insufficient permissions")
 }
 
 func (service *AuthService) getUser(username string) (cf.User, error) {
+	tag := "AuthService.getUser"
 	query := url.Values{}
 	query.Add("username", username)
 
 	users, err := service.client.ListUsersByQuery(query)
 	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error listing users by query: %v", err))
 		return cf.User{}, err
 	}
 
 	user := users.GetUserByUsername(username)
 	if len(user.Guid) == 0 {
+		service.logger.Error(tag, "No such user found")
 		return cf.User{}, fmt.Errorf("no such user")
 	}
 
@@ -67,10 +97,12 @@ func (service *AuthService) getUser(username string) (cf.User, error) {
 }
 
 func (service *AuthService) getUserRoles(user cf.User) ([]cf.V3Role, error) {
+	tag := "AuthService.getUserRoles"
 	roleQuery := url.Values{}
 	roleQuery.Add("user_guids", user.Guid)
 	roles, err := service.client.ListV3RolesByQuery(roleQuery)
 	if err != nil {
+		service.logger.Error(tag, fmt.Sprintf("Error listing V3 roles by query: %v", err))
 		return nil, err
 	}
 
