ca05651228882fc1cde125815f6c55156d1dafde
Ioannis Tsouvalas <tsouvalasi@atic.gr>
2024-06-10 11:04:30 +0000

    feat(auth_service): Enhance Verify method to check
    - Enhanced the Verify method to include a check
    for the cloud_controller.admin scope in JWT
    tokens. @itsouvalas
    - Added JWTClaims struct to represent claims in
    JWT tokens. @itsouvalas
    - Implemented DecodeJWT function to decode JWT
    tokens and extract claims. @itsouvalas
    - Introduced getTokenScopes function to extract
    scopes from a JWT token. @itsouvalas
    - Updated logging within the Verify method to
    provide detailed information about the
    verification process and errors. @itsouvalas

--- cf/auth_service.go
+++ cf/auth_service.go
@@ -1,6 +1,8 @@
 package cf
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
@@ -62,10 +64,17 @@ func (service *AuthService) Verify(auth string) error {
 	service.logger.Info(tag, fmt.Sprintf("Token scopes obtained: %v", tokenScopes))
 
 	// Check all the roles, but return good early if we find one that works.
+
+	// Check token scopes for cloud_controller.admin
+	for _, scope := range tokenScopes {
+		if scope == "cloud_controller.admin" {
+			service.logger.Info(tag, "User has cloud_controller.admin scope")
+			return nil
+		}
+	}
+
 	// Check CF roles for space_manager or space_developer
 	for _, role := range roles {
-		// NOTE: we should definitely be checking space IDs, too, but that's tomorrow
-		// guy's problem.
 		if role.Type == "space_manager" || role.Type == "space_developer" {
 			service.logger.Info(tag, fmt.Sprintf("User has role: %s", role.Type))
 			return nil
@@ -156,4 +165,49 @@ func getBearer(auth string) (string, error) {
 	}
 
 	return parts[bearerLoc+1], nil
+}
+
+// JWTClaims represents the claims in the JWT token
+type JWTClaims struct {
+	Scope []string `json:"scope"`
+}
+
+// DecodeJWT decodes the JWT token and extracts the claims
+func DecodeJWT(token string) (*JWTClaims, error) {
+	parts := strings.Split(token, ".")
+	if len(parts) < 3 {
+		return nil, fmt.Errorf("invalid token format")
+	}
+
+	payload := parts[1]
+	payloadDecoded, err := base64.RawURLEncoding.DecodeString(payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode payload: %v", err)
+	}
+
+	var claims JWTClaims
+	err = json.Unmarshal(payloadDecoded, &claims)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal claims: %v", err)
+	}
+
+	return &claims, nil
+}
+
+func getTokenScopes(auth string, logger Logger) ([]string, error) {
+	tag := "AuthService.getTokenScopes"
+	bearer, err := getBearer(auth)
+	if err != nil {
+		logger.Error(tag, fmt.Sprintf("Error getting bearer token: %v", err))
+		return nil, err
+	}
+
+	claims, err := DecodeJWT(bearer)
+	if err != nil {
+		logger.Error(tag, fmt.Sprintf("Error decoding JWT token: %v", err))
+		return nil, err
+	}
+
+	logger.Info(tag, fmt.Sprintf("Scopes found in token: %v", claims.Scope))
+	return claims.Scope, nil
 }
