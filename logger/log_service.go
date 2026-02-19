78c21aaecfef7eb6c29f72911f8a137bf3aed7a3
Norman Abramovitz <nabramovitz@fivetwenty.io>
2026-02-19 13:26:34 -0800

    Add missing logrus logging levels to LogService
    Extend the LogService interface and implementation with the four
    missing built-in logrus levels: Trace, Warn, Fatal, and Panic.
    Also update the LOG_LEVEL environment variable switch to support
    all seven levels.

--- logger/log_service.go
+++ logger/log_service.go
@@ -20,12 +20,20 @@ func New() *LogService {
 	// Set log level from environment variable
 	level := os.Getenv("LOG_LEVEL")
 	switch strings.ToLower(level) {
+	case "trace":
+		log.SetLevel(logrus.TraceLevel)
 	case "debug":
 		log.SetLevel(logrus.DebugLevel)
 	case "info":
 		log.SetLevel(logrus.InfoLevel)
+	case "warn":
+		log.SetLevel(logrus.WarnLevel)
 	case "error":
 		log.SetLevel(logrus.ErrorLevel)
+	case "fatal":
+		log.SetLevel(logrus.FatalLevel)
+	case "panic":
+		log.SetLevel(logrus.PanicLevel)
 	default:
 		log.SetLevel(logrus.InfoLevel) // Default to Info level
 	}
@@ -33,14 +41,30 @@ func New() *LogService {
 	return &LogService{log: log}
 }
 
+func (service *LogService) Trace(context string, message string) {
+	service.log.WithField("context", context).Trace(message)
+}
+
+func (service *LogService) Debug(context string, message string) {
+	service.log.WithField("context", context).Debug(message)
+}
+
 func (service *LogService) Info(context string, message string) {
 	service.log.WithField("context", context).Info(message)
+}
+
+func (service *LogService) Warn(context string, message string) {
+	service.log.WithField("context", context).Warn(message)
 }
 
 func (service *LogService) Error(context string, message string) {
 	service.log.WithField("context", context).Error(message)
 }
 
-func (service *LogService) Debug(context string, message string) {
-	service.log.WithField("context", context).Debug(message)
+func (service *LogService) Fatal(context string, message string) {
+	service.log.WithField("context", context).Fatal(message)
+}
+
+func (service *LogService) Panic(context string, message string) {
+	service.log.WithField("context", context).Panic(message)
 }
